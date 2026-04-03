93577133c2d14ebf6307bdcffd8e8597f5943ba3
AK12210 <156789576+AK12210@users.noreply.github.com>
2026-04-03 11:10:40 +0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\FavoriteController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::post('/ratings', [RatingController::class, 'store']);
+    Route::delete('/ratings/{recipeId}', [RatingController::class, 'destroy']);
+    Route::post('/favorites/{recipeId}', [FavoriteController::class, 'store']);
+    Route::delete('/favorites/{recipeId}', [FavoriteController::class, 'destroy']);
 });
